24cced15b172644187e6693108cc1d9ee2f256f7
Alan Tang <jmtangcs@gmail.com>
2025-09-16 21:46:42 +0800

    implement the lsm iterator and fused iterator

--- mini-lsm-starter/src/lsm_storage.rs
+++ mini-lsm-starter/src/lsm_storage.rs
@@ -30,6 +30,7 @@ use crate::compact::{
     CompactionController, CompactionOptions, LeveledCompactionController, LeveledCompactionOptions,
     SimpleLeveledCompactionController, SimpleLeveledCompactionOptions, TieredCompactionController,
 };
+use crate::iterators::merge_iterator::MergeIterator;
 use crate::lsm_iterator::{FusedIterator, LsmIterator};
 use crate::manifest::Manifest;
 use crate::mem_table::MemTable;
@@ -388,9 +389,21 @@ impl LsmStorageInner {
     /// Create an iterator over a range of keys.
     pub fn scan(
         &self,
-        _lower: Bound<&[u8]>,
-        _upper: Bound<&[u8]>,
+        lower: Bound<&[u8]>,
+        upper: Bound<&[u8]>,
     ) -> Result<FusedIterator<LsmIterator>> {
-        unimplemented!()
+        let snapshot = {
+            let guard = self.state.read();
+            let old_state = guard.clone();
+            old_state
+        };
+        let mut iters = vec![];
+        iters.push(Box::new(snapshot.memtable.scan(lower, upper)));
+        for imm_memtable in snapshot.imm_memtables.iter() {
+            iters.push(Box::new(imm_memtable.scan(lower, upper)));
+        }
+        let lsm_iter = LsmIterator::new(MergeIterator::create(iters)).unwrap();
+        let fused_iter = FusedIterator::new(lsm_iter);
+        Ok(fused_iter)
     }
 }
